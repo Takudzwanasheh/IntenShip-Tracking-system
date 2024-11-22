cbd9421fac4a15823bbaf3e6a6fc3f70f9626359
Takudzwanasheh <takudzwanashengaiso@gmail.com>
2024-11-22 01:25:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// alert("welcome");
+document.querySelector("#join").onclick=function (){
+    document.location.href="login.html";
+}
+
+// document.addEventListener('click', add);
+// function add(){
+//     document.getElementById('h1').innerHTML="done";
+// }
